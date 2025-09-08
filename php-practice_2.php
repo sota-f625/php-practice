34a53b20d26d04bfd4bdbde90423362936602041
sota-f625 <s7716xf625@gmail.com>
2025-09-08 10:42:48 +0900

    Q1.tic-tac問題

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,8 +1,25 @@
 <?php
 // Q1 tic-tac問題
 
+function number() {
+    for($x = 1; $x <= 100; $x++) {
+        if($x % 4 === 0 && $x % 5 === 0) {
+            echo "tic-tac" . "\n";
+        } elseif($x % 4 === 0) {
+            echo "tic" . "\n";
+        } elseif($x % 5 === 0) {
+            echo "tac" . "\n";
+        } else {
+            echo $x . "\n";
+        }
+    }
+
+}
+
+number();
 
 // Q2 多次元連想配列
+
 
 
 // Q3 オブジェクト-1
